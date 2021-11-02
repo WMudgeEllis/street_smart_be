e2848fc6f2fe286cb635e447f27112250defe1a1
Weston Ellis <ellisweston112@gmail.com>
2021-11-02 10:30:02 -0600

    downgrade?

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
@@ -22,7 +20,7 @@ Bundler.require(*Rails.groups)
 module StreetSmartBe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 5.2
 
     # Configuration for the application, engines, and railties goes here.
     #
