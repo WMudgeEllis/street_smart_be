3c433b19f6344405dfae43273a6d8f226ff5871c
Rteske <ryanteske@outlook.com>
2021-11-03 08:44:57 -0700

    Add model tests and factories

---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    created_at { Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all) }
+    updated_at { Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all) }
+    has_many :hazards
+  end
+end
