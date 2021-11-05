44471e1b711079dfb2f3b012a27a9efffb1717c3
Rteske <ryanteske@outlook.com>
2021-11-04 20:57:40 -0700

    Factories setup

--- spec/factories/hazard.rb
+++ spec/factories/hazard.rb
@@ -2,10 +2,8 @@ FactoryBot.define do
   factory :hazard do
     title { Faker::Company.name }
     description { Faker::Company.buzzword }
-    category { Faker::Marketing.buzzword }
-    longitude { Faker::Number.decimal(l_digits: 2, r_digits: 6) }
-    latitude { Faker::Number.decimal(l_digits: 2, r_digits: 6) }
-    belongs_to :user
-    has_one :hazard
+    category { 1 }
+    longitude { Faker::Number.decimal(l_digits: 2, r_digits: 4) }
+    latitude { Faker::Number.decimal(l_digits: 2, r_digits: 4) }
   end
 end
