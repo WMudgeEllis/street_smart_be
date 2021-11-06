3d8bbf52368857e7abb7e8622955d5c9bb6ce84f
Rteske <ryanteske@outlook.com>
2021-11-06 09:38:01 -0700

    Start on nearest hazards

--- app/models/hazard.rb
+++ app/models/hazard.rb
@@ -6,5 +6,12 @@ class Hazard < ApplicationRecord
 
   validates_presence_of :title, :description, :category, :latitude, :longitude, :user_id
 
+  acts_as_mappable
+
   enum category: %i[inanimate animate]
+
+
+  def self.nearest_hazards(current_location, limit)
+    self.within(50, origin: :current_location)
+  end
 end
--- spec/models/hazard_spec.rb
+++ spec/models/hazard_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Hazard do
     it { should belong_to(:user)}
     it { should have_one(:vote) }
   end
+
+  describe 'class methods' do
+    describe '#nearest_hazards(current_location, limit)' do
+      @user1 = create(:user)
+      @user2 = create(:user)
+      @user1_hazards = create_list(10, :hazard, user: @user1)
+      @user2_hazards = create_list(10, :hazard, user: @user2)
+      @current_location = [39.5296,119.8138]
+
+      expect()
+    end
+  end
 end
