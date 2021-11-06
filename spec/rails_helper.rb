50561005f997a066e996452e2566a523d32f3bfe
Andrew Massey <acmassey@ncsu.edu>
2021-11-06 13:17:51 -0400

    Add vcr configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -100,6 +100,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data('my api key') { ENV['safety_data_key'] }
+  config.filter_sensitive_data('<api_key>') { ENV['walk_api_key'] }
   config.configure_rspec_metadata!
 end
