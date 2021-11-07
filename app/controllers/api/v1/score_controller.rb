2da787c3187606c9ab7ad48929eea78572b16065
Andrew Massey <acmassey@ncsu.edu>
2021-11-07 17:40:13 -0500

    Update score controller

--- app/controllers/api/v1/score_controller.rb
+++ app/controllers/api/v1/score_controller.rb
@@ -2,13 +2,16 @@ class Api::V1::ScoreController < ApplicationController
   # before_action :
 
   def index
-    ScoreFacade.walk_safety_info(attrs)
+    # ScoreFacade.walk_safety_info(attrs)
+    attrs
   end
 
 
   private
   def attrs
+    # require "pry"; binding.pry
+    locations = Geocoder.search(params[:ip])
     require "pry"; binding.pry
-    location = Geokit::Geocoders::IpGeocoder.geocode('12.215.42.19')
+    coords = locations.first.coordinates
   end
 end
