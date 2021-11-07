764cf81ff45bc505438ff9ef252ac1087d1472c0
Andrew Massey <acmassey@ncsu.edu>
2021-11-07 17:41:24 -0500

    Write test

--- spec/requests/api/v1/walk_score_request_spec.rb
+++ spec/requests/api/v1/walk_score_request_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Walk score request' do
-  it 'returns the walk score' do
-    get '/api/v1/walkscore'
-
+  it 'returns the walk score', :vcr do
+    get '/api/v1/walkscore', params: { ip: '71.196.130.162' }
   end
 end
