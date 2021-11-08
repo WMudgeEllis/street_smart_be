0891565d38f3939fdd4a70ee5227530cc26040df
Andrew Massey <acmassey@ncsu.edu>
2021-11-08 17:19:47 -0500

    Remove blank call to serializer

--- app/controllers/api/v1/score_controller.rb
+++ app/controllers/api/v1/score_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ScoreController < ApplicationController
 
   def index
     score = ScoreFacade.walk_safety_info(attrs)
-    render json: 
   end
 
 
@@ -10,6 +9,7 @@ class Api::V1::ScoreController < ApplicationController
   def attrs
     locations = Geocoder.search(params[:ip])
     coords = locations.first.coordinates
+    require "pry"; binding.pry
     addresses = Geocoder.search(coords)
     addr_hash = addresses.first.data['address']
     current_address = "#{addr_hash['house_number']}%20#{addr_hash['road']}%20#{addr_hash['city']}%20#{addr_hash['state']}%20#{addr_hash['postcode']}"
