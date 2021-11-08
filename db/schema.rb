77812e3cf367e74fc7a0bb32b35e741865d491db
Rteske <ryanteske@outlook.com>
2021-11-08 07:21:39 -0800

    Add email column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_202922) do
+ActiveRecord::Schema.define(version: 2021_11_08_145322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_11_06_202922) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "votes", force: :cascade do |t|
