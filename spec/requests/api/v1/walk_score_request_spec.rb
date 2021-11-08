a03353263bc0336b220825084344443b74a24efb
Andrew Massey <acmassey@ncsu.edu>
2021-11-08 17:19:01 -0500

    Update spec to include my IP address

--- spec/requests/api/v1/walk_score_request_spec.rb
+++ spec/requests/api/v1/walk_score_request_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Walk score request' do
   it 'returns the walk score', :vcr do
-    get '/api/v1/walkscore', params: { ip: '71.196.130.162' }
+    get '/api/v1/walkscore', params: { ip: '69.132.37.48' }
   end
 end
