0ef95ae461c8fa96afdb6f122dadda03e6ba26e5
Weston Ellis <84806907+WMudgeEllis@users.noreply.github.com>
2021-11-08 09:47:21 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,14 @@ ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-user = User.create!
-hazard = user.hazards.create!(title: "Cheyenne, Wyoming", description: "Je suis comme je suis", latitude: "41.1400", longitude: "-104.8202", category: 0)
-hazard2 = user.hazards.create!(title: "North Pole, Alaska", description: "Je suis fait comme ça", latitude: "64.7511", longitude: "-147.3494", category: 1)
-hazard3 = user.hazards.create!(title: "Parking Lot", description: "is super sketch", latitude: "41.137859", longitude: "-104.766779", category: 1)
-hazard4 = user.hazards.create!(title: "Big 'ol pothole", description: "Like SUPER big", latitude: "41.143200", longitude: "-104.772970", category: 1)
+user1 = User.create(email: 'ellisweston112@gmail.com')
+user2= User.create(email: 'bummer@bummmer.com')
+hazard1 = user1.hazards.create!(title: "Cheyenne, Wyoming", description: "Je suis comme je suis", latitude: "41.1400", longitude: "-104.8202", category: 0)
+hazard2 = user1.hazards.create!(title: "North Pole, Alaska", description: "Je suis fait comme ça", latitude: "64.7511", longitude: "-147.3494", category: 1)
+hazard3 = user1.hazards.create!(title: "Parking Lot", description: "is super sketch", latitude: "41.137859", longitude: "-104.766779", category: 1)
+hazard4 = user1.hazards.create!(title: "Big 'ol pothole", description: "Like SUPER big", latitude: "41.143200", longitude: "-104.772970", category: 1)
 
-Vote.create!(hazard_id: hazard.id, upvote: 10, downvote: 1)
+Vote.create!(hazard_id: hazard1.id, upvote: 10, downvote: 1)
 Vote.create!(hazard_id: hazard2.id, upvote: 10, downvote: 1)
 Vote.create!(hazard_id: hazard3.id, upvote: 10, downvote: 1)
 Vote.create!(hazard_id: hazard4.id, upvote: 10, downvote: 1)
