6f4463f80138bdadc7db86c80f668bb7eb2e6512
Andrew Massey <acmassey@ncsu.edu>
2021-11-08 18:42:25 -0500

    Create score poro

--- app/poros/score.rb
+++ app/poros/score.rb
@@ -1,9 +1,11 @@
 class Score
   attr_reader :score,
-              :description
-  
+              :description,
+              :id
+
   def initialize(attrs)
     @score = attrs[:walkscore]
     @description = attrs[:description]
+    @id = nil
   end
 end
