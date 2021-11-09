962ffd32af966b7826526705d1779af5554dc308
Erin Quinn <equinn125@gmail.com>
2021-11-09 15:15:16 -0700

    adding some more testing

--- spec/requests/api/v1/votes/vote_update_request_spec.rb
+++ spec/requests/api/v1/votes/vote_update_request_spec.rb
@@ -7,11 +7,17 @@ RSpec.describe 'Vote Update' do
       @user1 = create(:user, email: 'test@email.com')
       @user2 = create(:user, email: 'test2@email.com')
       @hazard1 = create(:hazard, user: @user1)
+      @hazard2 = create(:hazard, user: @user1)
       @vote1 = create(:vote, hazard: @hazard1)
+      @vote2 = create(:vote, hazard: @hazard2)
       @user2vote = create(:user_vote, user: @user2, vote: @vote1)
       @vote_data = {
         upvote: 34,
         downvote: 55
+      }
+      @vote_data1 = {
+        upvote: 55,
+        downvote: 34
       }
     end
 
@@ -25,6 +31,17 @@ RSpec.describe 'Vote Update' do
     it 'does not allow a user to vote more than once on a hazard' do
         put "/api/v1/votes/#{@vote1.id}", params: {vote_data: @vote_data, email:@user2.email}
         expect(response.status).to eq(404)
+    end
+
+    it 'returns a 404 with a invalid id' do
+      put "/api/v1/votes/0", params: {vote_data: @vote_data, email:@user1.email}
+      expect(response.status).to eq(404)
+    end
+
+    it 'allows users to vote on a different hazard' do
+      put "/api/v1/votes/#{@vote2.id}", params: {vote_data: @vote_data1, email:@user2.email}
+      expect(response.status).to eq(204)
+      expect(response).to be_successful
     end
   end
 end
