7896ba7db4a717fead64287e72dbb31fac984cfc
Erin Quinn <equinn125@gmail.com>
2021-11-09 15:04:42 -0700

    tenative one vote method created

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -3,12 +3,11 @@ class Api::V1::VotesController < ApplicationController
     vote = Vote.find(params[:id])
     user = User.find_by(email: params[:email])
     user_vote = UserVote.find_by(user_id:user.id, vote_id: vote.id)
-    if !user_vote
+    if user_vote.nil?
       vote.update(vote_params)
       UserVote.create(user_id: user.id, vote_id: vote.id)
-      require "pry"; binding.pry
       render json: 200, status: 204
-    elsif user_vote
+    elsif !user_vote.nil?
       render json: 404, status: 404
     else
       render json: 404, status: 404
--- spec/requests/api/v1/votes/vote_update_request_spec.rb
+++ spec/requests/api/v1/votes/vote_update_request_spec.rb
@@ -4,13 +4,11 @@ require 'rails_helper'
 RSpec.describe 'Vote Update' do
   describe 'PUT api/v1/votes/:id' do
     before :each do
-      @user1 = create(:user)
-      @user2 = create(:user)
-      @user3 = create(:user)
+      @user1 = create(:user, email: 'test@email.com')
+      @user2 = create(:user, email: 'test2@email.com')
       @hazard1 = create(:hazard, user: @user1)
       @vote1 = create(:vote, hazard: @hazard1)
-      @user1vote = create(:user_vote, user: @user1, vote: @vote1)
-      @user1vote1 = create(:user_vote, user: @user2, vote: @vote1)
+      @user2vote = create(:user_vote, user: @user2, vote: @vote1)
       @vote_data = {
         upvote: 34,
         downvote: 55
@@ -18,15 +16,15 @@ RSpec.describe 'Vote Update' do
     end
 
     it 'returns a 204 no content on a sucessful request' do
-      put "/api/v1/votes/#{@vote1.id}", params: @vote_data
-      require "pry"; binding.pry
+      put "/api/v1/votes/#{@vote1.id}", params: {vote_data: @vote_data, email:@user1.email}
       expect(response.status).to eq(204)
       expect(response).to be_successful
       # This method works we cant figure out why rspec is a dick and wont let us test the vote object
     end
 
     it 'does not allow a user to vote more than once on a hazard' do
-
+        put "/api/v1/votes/#{@vote1.id}", params: {vote_data: @vote_data, email:@user2.email}
+        expect(response.status).to eq(404)
     end
   end
 end
