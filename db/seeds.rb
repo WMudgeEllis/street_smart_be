aa12abb1768436c8ff13dbca9bee2da6141f416a
Weston Ellis <84806907+WMudgeEllis@users.noreply.github.com>
2021-11-10 10:13:07 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+UserVote.destroy_all
 Vote.destroy_all
 Hazard.destroy_all
 User.destroy_all
