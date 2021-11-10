d93d8a76b195f9e99b6bf59033ad8a4ab75e9c4e
Ida Olson <idaolson@gmail.com>
2021-11-10 11:00:25 -0700

    Remove street address from private attrs method

--- app/controllers/api/v1/score_controller.rb
+++ app/controllers/api/v1/score_controller.rb
@@ -13,13 +13,9 @@ class Api::V1::ScoreController < ApplicationController
     ## but it can be refactored to get that info from the right param/method
     locations = Geocoder.search(params[:ip])
     coords = locations.first.coordinates
-    addresses = Geocoder.search(coords)
-    addr_hash = addresses.first.data['address']
-    current_address = "#{addr_hash['house_number']}%20#{addr_hash['road']}%20#{addr_hash['city']}%20#{addr_hash['state']}%20#{addr_hash['postcode']}"
-    {
+      {
       latitude: coords[0],
-      longitude: coords[1],
-      address: current_address
+      longitude: coords[1]
     }
   end
 end
