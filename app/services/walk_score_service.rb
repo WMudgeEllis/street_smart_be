9690aa30fa80b1453b4adf02ef7440a9a861dcf9
Erin Quinn <83930724+equinn125@users.noreply.github.com>
2021-11-10 17:39:44 -0700

    remove address from walk score servive

--- app/services/walk_score_service.rb
+++ app/services/walk_score_service.rb
@@ -2,7 +2,7 @@ class WalkScoreService
   class << self
 
     def walk_score_by_location(attrs)
-      url = "?format=json&lat=#{attrs[:latitude]}&lon=#{attrs[:longitude]}&address=#{attrs[:address]}"
+      url = "?format=json&lat=#{attrs[:latitude]}&lon=#{attrs[:longitude]}"
       WalkScoreClient.fetch(url)
     end
   end
